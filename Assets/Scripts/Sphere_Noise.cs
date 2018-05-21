9b3db93314193698cbaf233a6efe43d8a8ee93e6
James Mckay <s5068494@bournemouth.ac.uk>
2018-05-21 03:08:35 +0100

    Light

---
+++ Assets/Scripts/Sphere_Noise.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere_Noise : MonoBehaviour
+{
+    public AudioSource AS;
+
+     void OnCollisionEnter(Collision collision)
+     {
+        AS.Play();
+     }
+}
