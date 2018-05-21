5c4855e7596dd1edc513c127be9f9956a17b5457
James Mckay <s5068494@bournemouth.ac.uk>
2018-05-21 08:33:17 +0100

    Final

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play_button()
+    {
+        SceneManager.LoadScene("First", LoadSceneMode.Single);
+    }
+    public void Quit_button()
+    {
+        Application.Quit();
+    }
+}
